3b5094c8fc070dc388d2581f2f5df7a76127bc58
souhir2019 <47703877+souhir2019@users.noreply.github.com>
2019-03-31 18:03:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.interests i').on('mouseenter', (e) => {
+    $(e.currentTarget.nextSibling).toggleClass('show')
+})
+$('.interests i').on('mouseleave', (e) => {
+    $(e.currentTarget.nextSibling).toggleClass('show')
+})
+
